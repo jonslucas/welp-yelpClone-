595ee6dd3eb929ca638f87728de5bafdee6d848f
Jon <jon.s.lucas@gmail.com>
2016-06-12 16:08:12 -0400

    Fleshed out nearbySearch util.

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import Map, { GoogleApiWrapper } from 'google-maps-react'
 
+import { searchNearby } from 'utils/googleApiHelpers'
+
 export class Container extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            places: [],
+            pagination: null
+        }
+    }
+
+    onReady(mapProps, map) {
+        //Map ready to mount
+        const {google}  = this.props;
+        const opts = {
+            location: map.center,
+            radius: '500',
+            types: ['cafe']
+        };
+        searchNearby(google, map, opts)
+            .then((res, pagination)=> {
+                this.setState({
+                    places: res,
+                    pagination
+                })
+            }).catch((status, result)=> {
+            // there's an error in ma boot
+            })
+    }
+
     render() {
         return (
             <div>
                 Hello from Main Container!
                 <Map
-                    google={ this.props.google } />
+                    onReady={ this.onReady.bind(this) }
+                    google={ this.props.google }
+                    visible={ false } >
+                    { this.state.places.map(place=> {
+                        return (<div key={ place.id }>{ place.name }</div>)
+                    })}
+                </Map>
             </div>
         );
     };
