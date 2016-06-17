92767bfdad1fae1c112564efa6908d6cfed5726e
Jon <jon.s.lucas@gmail.com>
2016-06-17 17:52:07 -0400

    Added Marker component to Map view.

--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -1,15 +1,28 @@
 import React, { PropTypes } from 'react'
 import classnames from 'classnames'
-import Map from 'google-maps-react'
+import Map, { Marker } from 'google-maps-react'
 
 import styles from './styles.modules.css'
 
 export class MapComp extends React.Component {
+    renderMarkers() {
+        if (!this.props.places) return null;
+
+        return this.props.places.map(place=>{
+           return <Marker key={ place.id }
+                          name={ place.id }
+                          place={ place }
+                          position={ place.geometry.location }
+                          />
+        });
+    }
+
     render() {
         return (
             <Map google={ this.props.google }
                  className={ styles.map }
                  >
+                { this.renderMarkers() }
             </Map>
         );
     }
