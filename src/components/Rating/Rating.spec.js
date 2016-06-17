53d066abdd31ca8a1cbf3ef4238f8627c8978f16
Jon <jon.s.lucas@gmail.com>
2016-06-17 15:36:07 -0400

    Added Rating component tests.

--- src/components/Rating/Rating.spec.js
+++ src/components/Rating/Rating.spec.js
@@ -7,8 +7,23 @@ import styles from './styles.modules.css'
 
 describe('<Ratings />', ()=>{
     let wrapper;
+    const styleTop = `.${styles.top}`;
+    const styleBottom = `.${styles.bottom}`;
 
-    beforeEach(()=>{
-        wrapper = shallow(<Rating />)
+    it('fills the percentage as style', ()=>{
+        wrapper = shallow(<Rating percentage={0.10} />);
+        expect(wrapper.find(styleTop))
+            .to.have.style('width', '10%');
+
+        wrapper = shallow(<Rating percentage={0.99}/>)
+        expect(wrapper.find(styleTop))
+            .to.have.style('width', '99%');
+
+    });
+    it('renders bottom and top star meters', ()=>{
+        wrapper = shallow(<Rating percentage={0.99} />);
+        expect(wrapper.find(styleTop)).to.be.present;
+        expect(wrapper.find(styleBottom)).to.be.present;
+
     });
 });
